ba0b37a1ed39b38ec1ed345e17c9afca91758552
melvin <108800+MelvinTo@users.noreply.github.com>
2023-11-10 14:23:39 +0800

    refine

--- scripts/network_bandwidth.sh
+++ scripts/network_bandwidth.sh
@@ -47,10 +47,10 @@ main() {
         total_download_bps=$(expr "$final_download" - "$initial_download")
         total_upload_bps=$(expr "$final_upload" - "$initial_upload")
 
-        if [ "$total_download_bps" -gt 1073741824 ]; then
+        if [[ "$total_download_bps" -gt 1073741824 ]]; then
             output_download=$(echo "$total_download_bps 1024" | awk '{printf "%.1f \n", $1/($2 * $2 * $2)}')
             output_download_unit="G"
-        elif [ "$total_download_bps" -gt 1048576 ]; then
+        elif [[ "$total_download_bps" -gt 1048576 ]]; then
             output_download=$(echo "$total_download_bps 1024" | awk '{printf "%.1f \n", $1/($2 * $2)}')
             output_download_unit="M"
         else
@@ -58,10 +58,10 @@ main() {
             output_download_unit="K"
         fi
 
-        if [ "$total_upload_bps" -gt 1073741824 ]; then
+        if [[ "$total_upload_bps" -gt 1073741824 ]]; then
             output_upload=$(echo "$total_download_bps 1024" | awk '{printf "%.1f \n", $1/($2 * $2 * $2)}')
             output_upload_unit="G"
-        elif [ "$total_upload_bps" -gt 1048576 ]; then
+        elif [[ "$total_upload_bps" -gt 1048576 ]]; then
             output_upload=$(echo "$total_upload_bps 1024" | awk '{printf "%.1f \n", $1/($2 * $2)}')
             output_upload_unit="M"
         else
